93e85e75ae9d3b513f7aac3a5cc47d5436b3a8f8
Grayvox <144566632+Grayvox@users.noreply.github.com>
2025-05-07 19:58:19 -0400

    Add RNG for secret number

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_num = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    match io::stdin().read_line(&mut guess) {
+        Ok(_o) => {}
+        Err(_e) => {
+            println!("Failed to read line.")
+        }
+    }
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {guess}");
 }
