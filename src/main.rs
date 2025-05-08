15536f8b6544ad3d9c5263e9dd154c9836863333
Grayvox <144566632+Grayvox@users.noreply.github.com>
2025-05-07 23:17:02 -0400

    Finish game loop and match refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{cmp::Ordering, io};
+use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -6,16 +7,30 @@ fn main() {
 
     let secret_num = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    match io::stdin().read_line(&mut guess) {
-        Ok(_o) => {}
-        Err(_e) => {
-            println!("Failed to read line.")
+        match io::stdin().read_line(&mut guess) {
+            Ok(_) => {}
+            Err(_) => println!("Failed to read line."),
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
-
-    println!("You guessed: {guess}");
 }
